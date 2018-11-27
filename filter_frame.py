975f940a7a379b39566219f4fc51748374d9ff4b
charlesyn <charlesyniu@gmail.com>
2018-11-27 18:31:53 -0500

    Add perpendicular calculations, commented due to NaN bug

--- filter_frame.py
+++ filter_frame.py
@@ -8,7 +8,7 @@ class FilterFrame:
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         dst = cv2.calcBackProject([hsv], [0, 1], histogram, [0, 180, 0, 256], 1)
 
-        disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11,11))
+        disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
         dst = cv2.filter2D(dst, -1, disc, dst)
         erode_kernel = np.ones((3,3),np.uint8)
         dilate_kernel = np.ones((5,5), np.uint8)
@@ -26,16 +26,17 @@ class FilterFrame:
 
         for i in range(defects.shape[0]):
             s,e,f,d = defects[i,0]
-            sPrev, ePrev, fPrev, dPrev = defects[i - 1, 0]
-            sNext, eNext, fNext, dNext = defects[(i + 1) % defects.shape[0], 0]
+            sPrev, ePrev, fPrev, dPrev = defects[i - 2, 0]
+            sNext, eNext, fNext, dNext = defects[(i + 2) % defects.shape[0], 0]
             start = tuple(contour[s][0])
             nextStart = tuple(contour[sNext][0])
             prevStart = tuple(contour[sPrev][0])
             if self.findAngle(start, nextStart, prevStart) > 0.6:
                 end = tuple(contour[e][0])
                 far = tuple(contour[f][0])
+                #x1, y1, x2, y2 = self.getPerpendicular(prevStart, nextStart, start)
 
-                cv2.line(frame,start,end,[0,255,0],2)
+                #cv2.line(frame,(int(x1), int(y1)),(int(x2), int(y2)), [255,0,0],2)
                 frame = cv2.circle(frame,far,5,[0,0,255],-1)
 
         return frame
@@ -52,3 +53,17 @@ class FilterFrame:
         for i in range(len(a)):
             sum = sum + (a[i] - b[i])**2
         return sum**(1/2.0)
+
+    '''
+    def getPerpendicular(self, a, b, start):
+        if b[0] is a[0]:
+            return 0, 0, 0, 0
+        slope = (b[1] - a[1]) / (b[0] - a[0])
+        pSlope = slope * -1
+        y_intercept = start[1] - pSlope * start[0]
+        x1 = start[0] - 10
+        x2 = start[0] + 10
+        y1 = pSlope * x1 + y_intercept
+        y2 = pSlope * x2 + y_intercept
+        return [x1, y1, x2, y2]
+    '''
