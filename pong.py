4240ce493202a5e7b771424b0703383937b73bf9
Shashank Rajkumar <shashankrajkumar@gmail.com>
2018-11-27 18:27:32 -0500

    background_sub

--- pong.py
+++ pong.py
@@ -8,8 +8,12 @@ rows = 480
 cols = 640
 hs = hand_sample.HandSample(rows, cols)
 
+
+
 while True:
     ret, frame = cap.read()
+
+    
     original = frame.copy()
     hs.draw_hand_sample(frame)
     cv2.imshow('frame', frame)
@@ -22,6 +26,7 @@ ff = filter_frame.FilterFrame()
 
 while True:
     ret, frame = cap.read()
+    
     dst = ff.apply_filter(frame, histogram)
     im2, contours, hierarchy = cv2.findContours(dst, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     sortedContours = np.array(sorted(contours, key=cv2.contourArea))
